b5248d57fcea6a42e01a40b8f5623fb84fe683f1
John De Jesus <JDeJesus1@lahd.ci.la.ca.us>
2018-10-18 15:12:56 -0700

    navbar added with log in and company name

--- client/src/Components/Navbar/Navbar.js
+++ client/src/Components/Navbar/Navbar.js
@@ -1,26 +1,37 @@
-import React, {Component} from "react"
+import React, { Component } from "react"
 import "./Navbar.css"
 
 class Navbar extends Component {
-    render(){
-        return(
+    render() {
+        return (
             <div>
-                <nav class="navbar navbar-light navbar-expand-md navigation-clean-button">
-            <div class="container"><a class="navbar-brand" href="#">Company Name</a><button class="navbar-toggler" data-toggle="collapse" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
-                <div class="collapse navbar-collapse"
-                    id="navcol-1">
-                    <ul class="nav navbar-nav mr-auto">
-                        <li class="nav-item" role="presentation"><a class="nav-link active" href="#">First Item</a></li>
-                        <li class="nav-item" role="presentation"><a class="nav-link" href="#">Second Item</a></li>
-                        <li class="dropdown"><a class="dropdown-toggle nav-link dropdown-toggle" data-toggle="dropdown" aria-expanded="false" href="#">Dropdown </a>
-                            <div class="dropdown-menu" role="menu"><a class="dropdown-item" role="presentation" href="#">First Item</a><a class="dropdown-item" role="presentation" href="#">Second Item</a><a class="dropdown-item" role="presentation" href="#">Third Item</a></div>
-                        </li>
-                    </ul><span class="navbar-text actions"> <a href="#" class="login">Log In</a><a class="btn btn-light action-button" role="button" href="#">Sign Up</a></span></div>
-            </div>
-        </nav>
+                <nav className="navbar navbar-light navbar-expand-md navigation-clean-button">
+                    <div className="container">
+                        <a className="navbar-brand" href="#">Tea 4 Two</a>
+                        <button className="navbar-toggler" data-toggle="collapse" data-target="#navcol-1">
+                            <span className="sr-only">Toggle navigation</span>
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+                        <div>
+                        <span>logo place holder</span>
+                        </div>
+                        <div className="collapse navbar-collapse"
+                            id="navcol-1">
+                            <ul className="nav navbar-nav ml-auto">
+                                <li className="dropdown">
+                                    <a className="dropdown-toggle nav-link dropdown-toggle" data-toggle="dropdown" aria-expanded="false" href="#">Login </a>
+                                    <div className="dropdown-menu" role="menu">
+                                        <a href="#" className="btn btn-light action-button login" roll='button'>Log In</a>
+                                        <a className="btn btn-light action-button" role="button" href="#">Sign Up</a>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
             </div>
         )
     }
-} 
+}
 
 export default Navbar
